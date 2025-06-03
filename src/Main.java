45a2ea7a2517ce886c8c11c8e7d1ea674ac8ce3f
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-06-03 18:50:06 -0300

    Criação de teste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main{
     public static void main(String[] args) {
 
+//        Cliente c1 = new Cliente("Carlos","123","teste@email.com");
+//        System.out.println(c1.toString());
+//        c1.adicionarFavorito("Ex_Machina");
+//        System.out.println(c1.listarFavorito());
+//        c1.limparHistorico();
+//        System.out.println(c1.listarFavorito());
+//        c1.adicionarFavorito("Ex_Machina");
+//        System.out.println(c1.listarFavorito());
+//        c1.removerFavorito("Ex_Machina");
+//        System.out.println(c1.listarFavorito());
+//
     }
 }
