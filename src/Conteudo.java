66b69f22a240683fcce8310d1ad9e6d10d2f7806
Lucas Cunha da Silva <cdslucas07@gmail.com>
2025-06-03 17:45:18 -0300

    Atualização Classes Conteudo, Filme e Serie
    Declaração de construtores e declaração de getters e setters para cada campo.

--- src/Conteudo.java
+++ src/Conteudo.java
@@ -1,9 +1,51 @@
 public abstract class Conteudo {
-    private String Titulo;
+    private String titulo;
     private String ano;
     private String genero;
 
+    public Conteudo(String titulo, String ano, String genero) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+    }
 
+    public String getTitulo() {
+        return titulo;
+    }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public abstract String getTipo();
+
+    public abstract String getDetalhes();
+
+    @Override
+    public String toString() {
+        return String.format(
+                "%s (%s) - Gênero: %s - Tipo: %s - Detalhes: %s",
+                this.titulo,
+                this.ano,
+                this.genero,
+                this.getTipo(),
+                this.getDetalhes()
+        );
+    }
 }
