7f1f5ff3dc14214a8400a2a962e0c9d9766c9381
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-06-03 18:39:53 -0300

    Adicionei a lógica basica para os metodos do Usuario cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,33 +3,40 @@ import java.util.ArrayList;
 public class Cliente extends Usuario{
     ArrayList<String> favoritos;                //Alterar objeto para Conteudo
     ArrayList<String> historicoReproducao;      //Alterar objeto para Conteudo
-    public Cliente(String nome, String senha, String email, ArrayList favoritos, ArrayList historicoReproducao){
+    public Cliente(String nome, String senha, String email){
         super(nome, senha, email);
-        this.favoritos = favoritos;
-        this.historicoReproducao = historicoReproducao;
+        this.favoritos = new ArrayList<>();
+        this.historicoReproducao = new ArrayList<>();
     }
 
-    public String adicionarFavorito(){          //Alterar objeto para Conteudo
-        return "Adicionado aos Favoritos!";
+    public String adicionarFavorito(String filme) {  //Alterar objeto para Conteudo
+        favoritos.add(filme);
+        return filme + "adicionado aos Favoritos!";
     }
 
-    public String removerFavorito(){           //Alterar objeto para Conteudo
-        return "Removido dos Favoritos!";
+    public String removerFavorito(String filme){           //Alterar objeto para Conteudo
+        if (favoritos.contains(filme)) {
+            favoritos.remove(filme);
+            return filme + "removido dos Favoritos!";
+        } else {
+            return filme + "não está nos Favoritos!";
+        }
     }
 
     public String listarFavorito(){
-        return "Exibindo os Favoritos!";
+        return "Favoritos: " + favoritos;
     }
 
     public String registarReproducao(){
         return "REGISTRANDO...";
     }
 
-    public String limparHistorico(){
-        return "LIMPANDO...";
+    public void limparHistorico(){
+        favoritos.clear();
     }
 
-//    public String obterRecomendacao(){
+
+    //    public String obterRecomendacao(){
 //        return "Implementando...";
 //    }
 
--- src/Usuario.java
+++ src/Usuario.java
@@ -12,4 +12,33 @@ public abstract class Usuario {
     public String login(){
         return "Logado";
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + getNome() + "\nSenha: " + getSenha() + "\nEmail: " + getEmail();
+    }
 }
