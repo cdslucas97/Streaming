57cea7d7feb0c8ef193a123be80648845947c99d
Carlos Eduardo Wille Martins <you@example.com>
2025-06-03 22:32:45 -0300

    Todas funcionalidades basicas configuradas

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,15 +2,24 @@ public abstract class Usuario {
     private String nome;
     private String senha;
     private String email;
+    private boolean isBloqueado;
 
     public Usuario(String nome, String senha, String email){
         this.nome = nome;
         this.senha = senha;
         this.email = email;
+        this.isBloqueado = false;
     }
 
-    public String login(){
-        return "Logado";
+    public String login(String senhaInformada){
+        if(this.senha.equals(senhaInformada)){
+            if(this.isBloqueado){
+                return "Usuario bloqueado, entre em contato com o suporte";
+            }
+            return "Logado com sucesso";
+        } else{
+            return "Usuario ou senha invalido.";
+        }
     }
 
     public String getNome() {
@@ -37,8 +46,16 @@ public abstract class Usuario {
         this.email = email;
     }
 
+    public boolean isBloqueado() {
+        return isBloqueado;
+    }
+
+    public void setBloqueado(boolean bloqueado) {
+        this.isBloqueado = bloqueado;
+    }
+
     @Override
     public String toString() {
-        return "Nome: " + getNome() + "\nSenha: " + getSenha() + "\nEmail: " + getEmail();
+        return "Nome: " + getNome() + "\nSenha: " + getSenha() + "\nEmail: " + getEmail() + "Bloqueado: " + isBloqueado();
     }
 }
