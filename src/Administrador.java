ed1eb0f1d584e9d7be932301303fda1be0885ba0
Carlos Eduardo Wille Martins <you@example.com>
2025-06-03 23:15:49 -0300

    Melhoria dos metodos basicos. Criacao dos metodos adicionarUsuario() e desbloquearUsuario()

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,23 +1,61 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Administrador extends Usuario {
-
-    Scanner sc = new Scanner(System.in);
+    private ArrayList<Usuario> usuarios = new ArrayList<>();
 
     public Administrador(String nome, String senha, String email){
         super(nome,senha,email);
     }
 
-    public String visualizarUsuarios(){
-        return "VISUALIZANDO...";
+    public void adicionarUsuario(Usuario user){
+        if (user != null){
+            usuarios.add(user);
+        }
     }
 
-    public Usuario bloquearUsuario(Usuario user){
-        // Adicionar lógica para realizar switch - isBloqueado = true
-        return user;
+    public String listarUsuarios(){
+        if (usuarios.isEmpty()) {
+            return "Não há usuarios cadastrados no sistema";
+        }
+        return "Usuarios: " + usuarios;
     }
 
-    public void excluirUsuario(Usuario user){
-        System.out.println("");
+    public String bloquearUsuario(Usuario user){
+        if (user == null) {
+            return "Usuario invalido.";
+        }
+
+        if (usuarios.contains(user)) {
+            user.setBloqueado(true);
+            return "Usuario " + user.getNome() + " foi bloqueado";
+        } else {
+         return "Usuario nao encontrado";
+        }
+    }
+
+    public String desbloquearUsuario(Usuario user) {
+        if (user == null) {
+            return "Usuario invalido";
+        }
+
+        if (usuarios.contains(user)) {
+            user.setBloqueado(false);
+            return "Usuario " + user.getNome() + " foi desbloqueado";
+        } else {
+            return "Usuario nao foi encontrado";
+        }
+    }
+
+    public String excluirUsuario(Usuario user){
+        if (user == null) {
+            return "Usuario invalido";
+        }
+
+        if (usuarios.contains(user)) {
+            usuarios.remove(user);
+            return "Usuario " + user.getNome() + " foi excluido com sucesso";
+        } else {
+            return "Usuario nao encontrado";
+        }
     }
 }
