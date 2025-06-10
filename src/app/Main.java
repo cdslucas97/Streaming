e1bce72f3a238245d35d7883d6179f9eb48e2b46
Carlos Eduardo Wille Martins <you@example.com>
2025-06-10 19:05:02 -0300

    chore: criação de exemplos utilizando os metodos e classes principais

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,18 +1,33 @@
 package app;
+import domain.*;
+import exception.LoginInvalidoException;
 
 public class Main{
     public static void main(String[] args) {
 
-//        domain.Cliente c1 = new domain.Cliente("Carlos","123","teste@email.com");
-//        System.out.println(c1.toString());
-//        c1.adicionarFavorito("Ex_Machina");
-//        System.out.println(c1.listarFavorito());
-//        c1.limparHistorico();
-//        System.out.println(c1.listarFavorito());
-//        c1.adicionarFavorito("Ex_Machina");
-//        System.out.println(c1.listarFavorito());
-//        c1.removerFavorito("Ex_Machina");
-//        System.out.println(c1.listarFavorito());
-//
+        Cliente c1 = new Cliente("Carlos","123","carlos@email.com");
+        Administrador adm1 = new Administrador("CarlosAdm", "123","adm@email,com");
+        Serie serie1 = new Serie("O dia do chacal", "2024", "Thriller", 1);
+        Filme filme1 = new Filme("Ex_Machina", "2014", "Sci-Fi", 104);
+        Catalogo catalogo = new Catalogo();
+
+        adm1.adicionarUsuario(c1);
+        System.out.println(adm1.listarUsuarios());
+        adm1.adicionarConteudoAoCatalogo(serie1,catalogo);
+        adm1.removerConteudoDoCatalogo(filme1,catalogo);
+        adm1.bloquearUsuario(c1);
+        adm1.excluirUsuario(c1);
+
+        c1.adicionarFavorito(serie1);
+        c1.adicionarFavorito(filme1);
+        System.out.println(c1.listarFavorito());
+        c1.registarReproducao(filme1);
+        System.out.println(c1.listarHistorico());
+
+        try {
+            System.out.println(c1.login("senhaerrada"));
+        } catch (LoginInvalidoException e) {
+            System.err.println("Falha no login: " + e.getMessage());
+        }
     }
 }
